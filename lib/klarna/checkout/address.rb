97fc0d5d0cf4ef2ccb55be97b97fc758e6fbc376
João Anselmo <joao.anselmo2@gmail.com>
2014-11-07 16:11:07 +0100

    Adds 'Germany only' attributes to address.
    - They are not being included in the as_json method, need to test first
    if country specific fields needs to be filtered.

--- lib/klarna/checkout/address.rb
+++ lib/klarna/checkout/address.rb
@@ -2,7 +2,8 @@ module Klarna
   module Checkout
     class Address < Resource
       attr_accessor :given_name, :family_name, :care_of, :street_address,
-                    :postal_code, :city, :country, :email, :phone
+                    :postal_code, :city, :country, :email, :phone,
+                    :street_name, :street_number, :title
 
       def as_json
         json_sanitize({
