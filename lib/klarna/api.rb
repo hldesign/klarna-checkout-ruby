45913d00dd245d96751d735a64bc41eafe105b0d
João Anselmo <joao.anselmo2@gmail.com>
2015-03-17 14:23:35 +0100

    Missing file

---
+++ lib/klarna/api.rb
@@ -0,0 +1,10 @@
+require "klarna/api/activate"
+require "klarna/api/client"
+require "klarna/api/configuration"
+require "klarna/api/flags"
+
+module Klarna
+  module Api
+    extend Klarna::Api::Configuration
+  end
+end
